14301666255187e107a2ab72b0c6d87dcd59d291
gcharis <charis.giaralis@gmail.com>
2023-11-16 17:47:52 +0200

    Fix conftest

--- embeddings_engine/tests/conftest.py
+++ embeddings_engine/tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
-from embed.settings import get_settings
+from embeddings_engine.settings import get_settings
 from sqlmodel import Field, SQLModel, create_engine, text, Session
-from embed.main import app
+from embeddings_engine.main import app
 from fastapi.testclient import TestClient
 
 
