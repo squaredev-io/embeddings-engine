c4622ca776aff75b08d4f5c09bccff8abefb6795
gcharis <charis.giaralis@gmail.com>
2023-11-16 18:08:45 +0200

    Create file upload endpoint

--- embeddings_engine/v1/documents.py
+++ embeddings_engine/v1/documents.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends, status
 from embeddings_engine.schemas import Test
+from fastapi import FastAPI, File, UploadFile
+
 
 documents_router = APIRouter()
 
 
-@documents_router.get(
+@documents_router.post(
     "/items/{item_id}",
-    response_model=Test,
     summary="Get an item by id",
     status_code=status.HTTP_200_OK,
     # responses=add_error_responses([401, 404]),
     description="This endpoint allows you to get an item by giving its id.",
 )
-def get_item(item_id: int):
-    return Test(name="Charis")
+def get_item(file: UploadFile):
+    return {"file": file.filename, "content": "test"}
